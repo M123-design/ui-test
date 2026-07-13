926425f678ececf930bb861d97eea275df1b1cb8
Hanym <ninediv253@herojp.com>
2026-07-13 15:12:34 +0800

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,17 @@
-﻿import { defineConfig } from '@playwright/test';
-import baseConfig from '../playwright.config';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  ...baseConfig,
-  testDir: '.',
+  testDir: './tests',
+  timeout: 30000,
+  retries: 1,
+  use: {
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
 });
